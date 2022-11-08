972a040f883ba77c370338aba582be5a6db5d6d1
honsumal <14leealastair@gmail.com>
2022-11-07 21:51:55 -0500

    added seeds

---
+++ seeds/reactionSeeds.js
@@ -0,0 +1,13 @@
+const now = new Date()
+
+const reactionData = [
+    {reactionId: 1, reactionBody: 'xD', username: 'Meer', createdAt: now},
+    {reactionId: 2, reactionBody: ':D', username: 'Jtin', createdAt: now},
+    {reactionId: 3, reactionBody: ':)', username: 'Meer', createdAt: now},
+    {reactionId: 4, reactionBody: ':(', username: 'Bran', createdAt: now},
+    {reactionId: 5, reactionBody: ':\'(', username: 'Meer', createdAt: now},
+    {reactionId: 6, reactionBody: 'o.o', username: 'Nick', createdAt: now},
+    {reactionId: 7, reactionBody: '-.-', username: 'Pang', createdAt: now},
+]
+
+module.exports = reactionData
---
+++ seeds/userSeeds.js
@@ -0,0 +1,9 @@
+const userData = [
+    { username: 'Meer', email: 'psychicsurge@gmail.com' },
+    { username: 'Pang', email: 'grassysurge@gmail.com' },
+    { username: 'Nick', email: 'mistysurge@gmail.com' },
+    { username: 'Jtin', email: 'electricsurge@gmail.com' },
+    { username: 'Bran', email: 'volcanicsurge@gmail.com' },
+]
+
+module.exports = userData
