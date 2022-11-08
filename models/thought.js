0a7283b8db2d28f8be9511fd02cdc2dc6b589d36
honsumal <14leealastair@gmail.com>
2022-11-07 21:19:31 -0500

    added reaction schema

--- models/thought.js
+++ models/thought.js
@@ -1,5 +1,31 @@
+const { ObjectId } = require('bson');
 const { Schema, model } = require('mongoose');
 const {formatDate} = require('../utils/helpers')
+
+const reactionSchema = new Schema (
+    {
+        reactionId: {
+            type: ObjectId,
+            default: new ObjectId
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (date) => {
+                if (date) return formatDate(date)
+            } 
+        }
+    }
+)
 
 const thoughtSchema = new Schema(
     {
@@ -20,7 +46,7 @@ const thoughtSchema = new Schema(
             type: String,
             required: true,
         },
-        reactions: [{ type: Schema.Types.ObjectId, ref: 'reaction'}]
+        reactions: [reactionSchema]
     },
     {
         toJSON: {
